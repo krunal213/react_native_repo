a8a084b70ae59651b00e77135ca9f61ea2f917e7
krunal21041993 <kathikar.krunal3@gmail.com>
2025-12-07 09:07:24 +0530

    Worked On ViewComponent

--- App.tsx
+++ App.tsx
@@ -1,45 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <SafeAreaView>
+      <View style={styles.view1}>
+
+      </View>
+      <View style={styles.view2}>
+
+      </View>
+      
+    </SafeAreaView>
+    
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  view1:{
+    backgroundColor:"red",
+    height:100,
+    width:100
   },
-});
+  view2:{
+    backgroundColor:"yellow",
+    height:100,
+    width:100
+  }
+})
 
 export default App;
